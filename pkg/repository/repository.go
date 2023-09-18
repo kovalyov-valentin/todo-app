a1ed9ff3f05c5869984d89362becc528fc5d7026
kovalyov-valentin <kowalevvalen@yandex.ru>
2023-09-18 18:31:16 +0300

    implement repository

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,14 +1,14 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Authorization interface {
 }
 
 type TodoList interface {
-
 }
 
 type TodoItem interface {
-	
 }
 
 type Repository struct {
@@ -17,6 +17,6 @@ type Repository struct {
 	TodoItem
 }
 
-func NewRepository() *Repository {
+func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{}
 }
